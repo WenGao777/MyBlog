ca7ad7916a71c0e40542bbe750b0ecc6051b51fe
WenGao <1056040533@qq.com>
2018-04-21 19:52:24 +0800

    第一天
    完成了首页，不完整的留言和注册的界面

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
+        (function () {
+            window.scrollReveal = new scrollReveal({ reset: true });
+        })();
+    };
+});
